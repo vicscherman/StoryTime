64a180d6a90e47d7a12714b206cc1a76133c693f
vicscherman <vicscherman@gmail.com>
2021-03-13 19:23:34 -0500

    adding socket and some conditional logic

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,49 @@ const http = require ('http').createServer(app)
 const cors = require('cors')
 //set back end port to 5000 or whatever it ends up being when deployed
 const PORT = process.env.PORT || 5000
+
+const {addUser,getUser, deleteUser, getUsers, roomFull} =require('./users') 
+
+
+//initialize socket, pass the http const to it
+const io = require('socket.io')(http)
+
+
+
 //use cors for the app
 app.use(cors())
+//invoke on method w/ connection as the event name, and a callback. Callback holds that socket instance to listen to/emit events
+io.on('connection', (socket)=>{
+    //3 events to listen for, logging in, sending messages, disconnecting
+    socket.on('login', ({name, room}, callback) =>{
+
+    })
+
+    socket.on('sendMessage', message =>{
+
+    })
+
+    socket.on('disconnect', ()=> {
+
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //setting our home route, test message to display on screen
 app.get('/', (req, res)=>{
     res.send('Back end server running')
@@ -16,3 +57,4 @@ app.get('/', (req, res)=>{
 http.listen(PORT, () =>{
     console.log(`server listening on ${PORT}`)
 })
+
---
+++ backend/users.js
@@ -0,0 +1,41 @@
+//our logged in users will be an array
+//To do: set limits on number of users, set Interval to round robin through to active user
+const users = [];
+//takes in Socket ID, username, and room
+
+const addUser = (id, name, room) => {
+  //first check to see if the user already exists. Do this by username
+  const userExists = users.find(
+    (user) => user.name.trim.toLowerCase() === name.trim().toLowerCase()
+  );
+  //conditions for logging in with error messages
+  if (userExists) return { error: "User already exists!" };
+  if (!name && !room) return { error: "You need to input a room and username" };
+  if (!name || !room)
+    return { error: "Make sure to put in your username or room name" };
+  //define our user, push it to users array, return user
+  const user = { id, name, room };
+  users.push(user);
+  return { user };
+};
+// lets us get user based off id. Find a user where the user.id  is equal to the specified ID
+const getUser = (id) => {
+  let user = users.find((user) => user.id == id);
+  return user;
+};
+//delete users based on ID
+const deleteUser = (id) => {
+  //first find index of user where the id is equal to the deleteuser ID
+  const userIndex = users.findIndex((user) => user.id === id);
+  //if the userindex exists, we remove it from that 1 element from the array, return the 0th element
+  if (userIndex !== -1) return users.splice(userIndex, 1)[0];
+};
+
+const getUsers = (room) => users.filter((user) => user.room === room);
+
+//to check if our room is full. If the room exists and the length of the users array > 5 we  return
+const roomFull = (room) => {
+  if (room && users.length > 5) return { error: "Sorry that room is full!" };
+};
+
+module.exports = { addUser, getUser, deleteUser, getUsers, roomFull };
